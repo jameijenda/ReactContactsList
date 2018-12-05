e521194dceec1503679d56459c1626afa37d3a41
Manu <Manu@MacBook-Air-de-Majo.local>
2018-12-05 01:56:14 -0300

    Adding toggle button.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+
+  onShowClick = e => {};
+
   render() {
-    const { contact } = this.props;
+    const { name, email, phone } = this.props.contact;
+
     return (
       <div className="card card-body mb-3">
-        <h4>{contact.name}</h4>
+        <h4>
+          {name}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+          />
+        </h4>
+
         <ul className="list-group">
-          <li className="list-group-item">Email: {contact.email}</li>
-          <li className="list-group-item">Phone: {contact.phone}</li>
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     );
