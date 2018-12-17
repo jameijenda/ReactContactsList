d7cff959302413f136de9ca9640e03758349f753
Manu <Manu@MacBook-Air-de-Majo.local>
2018-12-16 21:30:57 -0300

    Added functionality to the dropdown button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,7 @@ class Contact extends Component {
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
@@ -22,11 +23,12 @@ class Contact extends Component {
             className="fas fa-sort-down"
           />
         </h4>
-
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
